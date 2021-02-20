4d7f349ac119ea0e13290bf145a092121b15936f
Yong SangYoon <64957267+ryong9rrr@users.noreply.github.com>
2021-02-21 05:33:26 +0900

    #2.0 Loading 구현

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from "react";
-import Movies from "./Movies";
+import React, { useState, useEffect } from "react";
+import GetMovies from "./GetMovies";
 
 const App = () => {
+  const { loading, movies } = GetMovies({
+    url:
+      "https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating",
+  });
   return (
     <div>
       <h1>Movie-app</h1>
-      <ul>
-        <Movies />
-      </ul>
+      <ul>{loading ? "Loading..." : movies}</ul>
     </div>
   );
 };
---
+++ src/GetMovies.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from "react";
+import defaultAxios from "axios";
+
+const GetMovies = (opts, axiosInstance = defaultAxios) => {
+  const [state, setState] = useState({
+    loading: true,
+    movies: null,
+    error: null,
+  });
+
+  useEffect(() => {
+    axiosInstance(opts)
+      .then((res) => {
+        setState({
+          ...state,
+          loading: false,
+          movies: res.data.data.movies.map((movie, index) => (
+            <li key={index}>{movie.title}</li>
+          )),
+        });
+      })
+      .catch((error) => {
+        setState({
+          ...state,
+          loading: true,
+          error,
+        });
+      });
+  }, []);
+
+  return { ...state };
+};
+
+export default GetMovies;
